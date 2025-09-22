1b8a670885c3919718bf5a895715ce98259709fb
Projector <231699516+Pr0jector23@users.noreply.github.com>
2025-09-22 16:48:48 +0400

    ...

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Pr0jector23.github.io/match-game-100/"
+  base: "./",
 })
